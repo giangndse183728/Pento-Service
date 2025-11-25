997885d1840dbbd00d9130d260ba92d84ab38faf
giangndse183728 <giangndse183728@fpt.edu.vn>
2025-11-26 03:37:00 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,6 @@ async function bootstrap() {
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(port);
+   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
